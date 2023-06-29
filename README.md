457703e258bd5e11da12f54d3d3135e8b7e77e47
Mert Ünlü <69141853+mertunlu@users.noreply.github.com>
2023-06-29 23:47:50 +0300

    react-leaflet plugin

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # react-leaflet-fallback
+
+react-leaflet-fallback is a plugin that provides a fallback tile layer implementation for [React Leaflet](https://react-leaflet.js.org/) maps. It allows you to load lower scaled tile images when originals are not found.
+
+## Installation
+
+You can install the package via npm:
+
+```bash
+npm install react-leaflet-fallback
+```
+
+## Usage
+
+Here's an example of how to use the `FallbackTileLayer` component:
+
+```jsx
+import React from 'react';
+import { FallbackTileLayer } from 'react-leaflet-fallback';
+
+// Inside your component
+const MyMapComponent = () => {
+  return (
+    <FallbackTileLayer
+      zoom={10}
+      minZoom={1}
+      maxZoom={15}
+      maxNativeZoom={15}
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="/api/map_tiles/4uMaps/{z}/{x}/{y}.png"
+      fallbackTileUrl="/api/map_tiles/4uMaps/{z}/{x}/{y}.png" // Fallback tile URL
+      errorTileUrl="/api/map_tiles/error.png" // Error tile URL
+    />
+  );
+};
+
+export default MyMapComponent;
+```
+
+Replace the `url` and `fallbackTileUrl` properties with your own tile URLs. You can also customize other properties such as `zoom`, `minZoom`, `maxZoom`, and `attribution` according to your needs.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or want to add new features, feel free to submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
---
+++ react-leaflet-fallback.js
@@ -0,0 +1,179 @@
+import L from 'leaflet';
+import { useEffect } from 'react';
+import {
+  useMap,
+  MapContainer,
+  TileLayer,
+  ZoomControl,
+} from "react-leaflet";
+
+
+L.TileLayer.Fallback = L.TileLayer.extend({
+    options: {
+      minNativeZoom: 0
+    },
+  
+    initialize: function (urlTemplate, options) {
+      L.TileLayer.prototype.initialize.call(this, urlTemplate, options);
+    },
+  
+    createTile: function (coords, done) {
+      var tile = L.TileLayer.prototype.createTile.call(this, coords, done);
+      tile._originalCoords = coords;
+      tile._originalSrc = tile.src;
+      tile._fallbackZoom = coords.z
+  
+      return tile;
+    },
+  
+    _createCurrentCoords: function (originalCoords) {
+      var currentCoords = this._wrapCoords(originalCoords);
+  
+      currentCoords.fallback = true;
+  
+      return currentCoords;
+    },
+  
+    _originalTileOnError: L.TileLayer.prototype._tileOnError,
+  
+    _tileOnError: function (done, tile, e) {
+      
+      console.log("tile-on-error is working now...\n")
+      console.log("original coords: ", originalCoords)
+      /*
+      console.log("current zoom level / fallback: \n", 
+        tile._fallbackZoom === undefined
+        ? originalCoords.z - 1
+        : tile._fallbackZoom - 1)
+*/
+      
+      var layer = this,
+        originalCoords = tile._originalCoords,
+        currentCoords = (tile._currentCoords =
+          tile._currentCoords || layer._createCurrentCoords(originalCoords)),
+        fallbackZoom =
+          tile._fallbackZoom === undefined
+            ? originalCoords.z - 1
+            : tile._fallbackZoom - 1,
+        scale = (tile._fallbackScale = (tile._fallbackScale || 1) * 2),
+        tileSize = layer.getTileSize(),
+        style = tile.style,
+        newUrl,
+        top,
+        left;
+          
+      console.log("original coords: ", originalCoords)
+        
+      //console.log("on-error is working now...")
+  
+      if (fallbackZoom < layer.options.minNativeZoom) {
+        // Handle the specific case of z/0/0 differently
+        if (originalCoords.x === 0 && originalCoords.y === 0) {
+          // Fallback behavior for coordinates (0, 0)
+          currentCoords.x = 1;
+          currentCoords.y = 1;
+          currentCoords.z = originalCoords.z - 1;
+        
+          newUrl = layer.getTileUrl(currentCoords);
+        } else {
+          return this._originalTileOnError(done, tile, e);
+        }
+        
+      }
+  
+      currentCoords.z = fallbackZoom;
+      currentCoords.x = Math.floor(currentCoords.x / 2);
+      currentCoords.y = Math.floor(currentCoords.y / 2);
+  
+      console.log("x, y, z coordinates after update: \n \d \n", currentCoords)
+      
+      // Decrement fallbackZoom for the next iteration -- this solved everything -- defines and updates the fallbackzoom.
+      tile._fallbackZoom = fallbackZoom;
+      
+      newUrl = layer.getTileUrl(currentCoords);
+  
+      style.width = tileSize.x * scale + 'px';
+      style.height = tileSize.y * scale + 'px';
+  
+      top = (originalCoords.y - currentCoords.y * scale) * tileSize.y;
+      style.marginTop = -top + 'px';
+      left = (originalCoords.x - currentCoords.x * scale) * tileSize.x;
+      style.marginLeft = -left + 'px';
+  
+      style.clip =
+        'rect(' +
+        top +
+        'px ' +
+        (left + tileSize.x) +
+        'px ' +
+        (top + tileSize.y) +
+        'px ' +
+        left +
+        'px)';
+  
+      layer.fire('tilefallback', {
+        tile: tile,
+        url: tile._originalSrc,
+        urlMissing: tile.src,
+        urlFallback: newUrl
+      });
+  
+      tile.src = newUrl;
+    },
+  
+    getTileUrl: function (coords) {
+      var z = coords.z = coords.fallback ? coords.z : this._getZoomForUrl();
+  
+      var data = {
+        r: L.Browser.retina ? '@2x' : '',
+        s: this._getSubdomain(coords),
+        x: coords.x,
+        y: coords.y,
+        z: z
+      };
+      if (this._map && !this._map.options.crs.infinite) {
+        var invertedY = this._globalTileRange.max.y - coords.y;
+        if (this.options.tms) {
+          data['y'] = invertedY;
+        }
+        data['-y'] = invertedY;
+      }
+  
+      return L.Util.template(this._url, L.extend(data, this.options));
+    }
+  });
+  
+  
+  // Create a factory function for the fallback tile layer
+  L.tileLayer.fallback = function (urlTemplate, options) {
+    return new L.TileLayer.Fallback(urlTemplate, options);
+  };
+
+  
+  const FallbackTileLayer = (props) => {
+    const { url, ...options } = props;
+    
+    const map = useMap();
+    
+    console.log("MAP VALUES IN FALLBACKTILELAYER: \n", map);
+  
+    useEffect(() => {
+      // Create the fallback tile layer using the factory function
+      const fallbackTileLayer = L.tileLayer.fallback(url, options);
+  
+      // Add the fallback tile layer to the Leaflet map
+      if (map && fallbackTileLayer) {
+        fallbackTileLayer.addTo(map);
+      } // Replace `map` with your Leaflet map instance
+  
+      return () => {
+        // Clean up: remove the fallback tile layer from the Leaflet map
+        fallbackTileLayer.removeFrom(map); // Replace `map` with your Leaflet map instance
+      };
+    }, [url, options]);
+  
+    return null; // This component doesn't render any JSX, so return null
+  };
+  
+  export default FallbackTileLayer;
+  
